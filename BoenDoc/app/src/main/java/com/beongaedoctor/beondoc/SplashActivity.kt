1e164d4da41a48b886679999237d21fa988beb59
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-03-24 01:47:45 +0900

    Splash, SignIn UI
    스플래시 화면, 로그인 화면 UI 구성

---
+++ BoenDoc/app/src/main/java/com/beongaedoctor/beondoc/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.beongaedoctor.beondoc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        showSplash()
+
+    }
+
+
+    private fun showSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+        finish()}, 3000)
+    }
+}
