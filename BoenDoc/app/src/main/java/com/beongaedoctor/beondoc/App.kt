c2f6de304e2e643120d4ed0d8d0cb2f2852b2c06
yangsooplus <69582122+yangsooplus@users.noreply.github.com>
2022-06-11 21:31:57 +0900

    코드정리중

--- BoenDoc/app/src/main/java/com/beongaedoctor/beondoc/App.kt
+++ BoenDoc/app/src/main/java/com/beongaedoctor/beondoc/App.kt
@@ -13,71 +13,78 @@ import java.security.Security
 class App : Application() {
 
     init{
-        instance = this
+        instance = this //App 클래스를 싱글톤 패턴으로 쓰기 위해 생성한 instance
     }
 
     companion object {
-        lateinit var prefs : Preference
-        lateinit var gson : Gson
+        lateinit var prefs : Preference //SharedPreference 사용을 위한 Preference 객체
+        lateinit var gson : Gson //Json 파싱을 위한 Gson 라이브러리 객체
 
-        var instance: App? = null
-        fun context() : Context {
-            return instance!!.applicationContext
+        var instance: App? = null //App 클래스 객체
+        fun context() : Context { //App.context()를 호출하면 앱의 모든 곳에서 앱의 context를 사용할 수 있다
+            return instance!!.applicationContext //context 반환
         }
 
-        var noMember = false
+        var noMember = false //비회원으로 기능 사용시 true.
     }
 
     override fun onCreate() {
-        prefs = Preference(applicationContext) // 다른 액티비티보다 먼저 생성되어야 데이터 전달 가능. onCreate보다 먼저 초기화 해야 함
-        gson = GsonBuilder().create()
-        super.onCreate()
-        Security.insertProviderAt(Conscrypt.newProvider(), 1);
+        prefs = Preference(applicationContext) // 다른 액티비티보다 먼저 생성되어야 데이터 전달 가능. 타 액티비티의 onCreate보다 먼저 초기화 해야 함
+        gson = GsonBuilder().create() //Gson 빌더를 빌드하여 바로 호출하여 사용할 수 있도록 함
+        super.onCreate() //onCreate()로 App 클래스 생성
+        Security.insertProviderAt(Conscrypt.newProvider(), 1) //OkHttp를 API 21 이하에서 사용하기 위해 작성
     }
 
 }
 
 
-class Preference (context: Context) {
+//많은 곳에서 SharedPreference를 사용하기 때문에 싱글톤 패턴으로 설계했다.
+class Preference (context: Context) { //SharedPreference 사용을 담당하는 클래스
     private val prefs : SharedPreferences = context.getSharedPreferences("shared",MODE_PRIVATE)
 
+    //Member 클래스는 직렬화 되어있어서 String으로 저장하고 불러온다.
+
+    //Member 클래스에 내용을 담아 저장한다.
     fun setMember(key: String, data:Member) {
-        val memberInfo = gson.toJson(data, Member::class.java)
-        prefs.edit().putString(key, memberInfo).apply()
+        val memberInfo = gson.toJson(data, Member::class.java) //Member 클래스 객체를 Json으로 변환
+        prefs.edit().putString(key, memberInfo).apply() //key와 매핑하여 생성된 Json을 저장한다.
     }
 
+    //key로 Member 클래스 객제를 리턴한다.
     fun getMember(key:String, default: String) : Member {
-        val gsonMemberInfo = prefs.getString(key, default)
-        if (gsonMemberInfo != null)
+        val gsonMemberInfo = prefs.getString(key, default) //key로 저장된 정보를 찾는다.
+        if (gsonMemberInfo != null)  //저장된 정보를 가져오면 Json을 Member로 변환하여 리턴
             return gson.fromJson(gsonMemberInfo, Member::class.java)
-        else
-            return Member()
+        else //혹시라도 저장된 정보가 없다면
+            return Member() //디폴트 객체를 반환한다.
     }
 
+    //key에 해당하는 데이터를 삭제한다.
     fun deleteByKey(key: String) {
         prefs.edit().remove(key).apply()
     }
 
+    //key-value 쌍으로 String 정보를 저장한다.
     fun setString(key:String, value:String) {
         prefs.edit().putString(key, value).apply()
     }
 
+    //key로 저장된 String 정보를 불러온다
     fun getString(key:String, default : String): String? {
         return prefs.getString(key, default)
     }
 
+    //key-value 쌍으로 Boolean 정보를 저장한다.
     fun setBool(key: String, value:Boolean) {
         prefs.edit().putBoolean(key, value).apply()
     }
 
+    //key로 저장된 Boolean 정보를 불러온다
     fun getBool(key: String, default: Boolean) : Boolean {
         return prefs.getBoolean(key, default)
     }
 }
 
-//val testMemberInfo = App.prefs.getString("memberInfo", "")
-//출처: https://leveloper.tistory.com/133 [꾸준하게]
-
-//user 정보 db에 저장.
+//App을 싱글톤 클래스로 사용했기 때문에 아래형식으로 모든 클래스에서 사용이 가능하다.
 //App.prefs.setMember("memberInfo", member)
 //App.prefs.getMember("memberInfo", "")
--- BoenDoc/app/src/main/java/com/beongaedoctor/beondoc/BasicExamActivity.kt
+++ BoenDoc/app/src/main/java/com/beongaedoctor/beondoc/BasicExamActivity.kt
@@ -22,12 +22,14 @@ class BasicExamActivity : AppCompatActivity() {
     // 매번 null 체크 하지 않도록 바인딩 변수 재선언
     private val binding get() = BEbinding!!
 
-    private var pageNum = 0
+    private var pageNum = 0 //현재 사용자가 보고 있는 페이지.
 
-    lateinit var member : Member
+    lateinit var member : Member //사용자 정보를 담을 Member 클래스
+    //Fragment를 쉽게 사용하기 위해 미리 초기화하여 배열에 담아두어 사용한다.
     private val fragmentArray = arrayOf(DrugFragment(), SocialFragment(), FamilyFragment(), PhysicalFragment(), WomanFragment())
-    var isMan = false
-    var isRevise = false
+
+    var isMan = false //남성이면 true, 여성이면 false
+    var isRevise = false //수정중이면 true, 초회 작성이면 false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +47,7 @@ class BasicExamActivity : AppCompatActivity() {
         //앞서 입력한 유저 정보에서 남성이면 여성력(산과력) PASS
         if (member.gender == 1L) isMan = true
 
-        //첫번째 페이지(DrugFragment) 셋팅
+        //첫번째 페이지(DrugFragment)를 화면에 띄워준다.
         supportFragmentManager.beginTransaction()
             .add(R.id.framelayout, fragmentArray[0], "drug")
             .commit()
@@ -66,56 +68,54 @@ class BasicExamActivity : AppCompatActivity() {
         //UI 갱신을 onCreateView 다음인 onStart 에서 시켜준다. onCreate 에서 하면 갱신이 안 된다.
         if (isRevise) { //수정 버전인 경우
             if (member.drug.isNotEmpty()) //저장된 정보가 있을 경우
-                fragmentArray[0].view?.findViewById<EditText>(R.id.drugedittext)?.setText(member.drug) //해당하는 editText 에 값 채운다
+                fragmentArray[0].view?.findViewById<EditText>(R.id.drugedittext)?.setText(member.drug) //해당하는 editText 에 값을 채워서 보여준다.
             if (member.social.isNotEmpty())
                 fragmentArray[1].view?.findViewById<EditText>(R.id.socialedittext)?.setText(member.social)
             if (member.family.isNotEmpty())
                 fragmentArray[2].view?.findViewById<EditText>(R.id.familyedittext)?.setText(member.family)
             if (member.trauma.isNotEmpty())
                 fragmentArray[3].view?.findViewById<EditText>(R.id.physicaledittext)?.setText(member.trauma)
-            //산과력은 string을 또 분해해야 하니 과감하게 포기한다.
         }
 
 
-        binding.nextBtn.setOnClickListener { //다음 버튼
+        binding.nextBtn.setOnClickListener { //다음 버튼 눌렀을 시 실행
             switchFragment(true)
         }
 
-        binding.prevBtn.setOnClickListener { //이전 버튼
+        binding.prevBtn.setOnClickListener { //이전 버튼 눌렀을 시 실행
             switchFragment(false)
         }
     }
 
     //다음, 이전 버튼을 눌렀을 경우 실행.
-    //next 가 참이면 다음 버튼을 누른 경우이다.
+    //next가 true면 다음 버튼을 누른 경우이다.
     private fun switchFragment( next : Boolean) {
 
         if (next && ((!isMan && pageNum == 4) || (isMan && pageNum == 3))) { //(여성+산과력) (남성+외상력) 입력 후 다음을 누르면 완료
-            setConfirmDialogue() //완료 한다는 Dialogue를 띄워준다 (추후 추가)
+            setData() //작성 완료
             return
         }
         else if (!next && pageNum < 1) //0페이지에서 이전을 누르면 무반응
             return
 
-        //----- 이상 특수한 경우 처리 완 -----
-
-
+        //현재 표시된 페이지 숨김
+        //사용자가 입력했던 정보를 유지하고 싶어서 프래그먼트를 생성 후 파괴시키지 않고 표시/숨김으로 처리했다.
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.hide(fragmentArray[pageNum]) //현재 페이지 숨김
+        transaction.hide(fragmentArray[pageNum])
 
         if (next) pageNum++ else pageNum-- //페이지 바꾸기
 
 
 
-        when (pageNum) {
-            0 -> { //drugFragment는 액티비티 생성 시 만들었음
+        when (pageNum) { //앞으로 표시할 페이지에 따라 실행
+            0 -> { //drugFragment는 액티비티 생성 시 만들었음. 숨겼던 프래그먼트를 보여준다.
                 transaction.show(fragmentArray[0])
             }
-            1 -> { //최초 문진 시 Fragment 생성
-                if (supportFragmentManager.findFragmentByTag("Social") == null){
-                    transaction.add(R.id.framelayout, fragmentArray[1], "Social").commit()
+            1 -> {
+                if (supportFragmentManager.findFragmentByTag("Social") == null){ //해당 페이지를 처음 보여줄 경우
+                    transaction.add(R.id.framelayout, fragmentArray[1], "Social").commit() //사회력을 태그를 붙여서 FragmentManager에 넣는다.
                 }
-                else {
+                else { //이미 보여준 적이 있으면 숨겼던 Fragment를 보여준다.
                     transaction.show(fragmentArray[1]).commit()
                 }
             }
@@ -150,7 +150,8 @@ class BasicExamActivity : AppCompatActivity() {
 
 
 
-    private fun setBasicExam()  { //문진 내용을 member 클래스 객체에 집어넣기
+    private fun setBasicExam()  { //문진 내용을 member 클래스 객체에 넣어서 저장하기
+        //약물 투약력, 사회력, 가족력, 과거력 및 외상력은 단순히 입력한 text를 저장하기만 하면 된다.
         member.drug = fragmentArray[0].requireView().findViewById<EditText>(R.id.drugedittext).text.toString()
         member.social = fragmentArray[1].requireView().findViewById<EditText>(R.id.socialedittext).text.toString()
         member.family = fragmentArray[2].requireView().findViewById<EditText>(R.id.familyedittext).text.toString()
@@ -158,11 +159,17 @@ class BasicExamActivity : AppCompatActivity() {
 
         if (isMan) return //남성인 경우 산과력은 PASS
 
+
+        //여성인 경우 산과력/여성력 Fragment에 대한 처리도 한다.
         val womanFragment = fragmentArray[4].requireView()
 
         //빈 string에 해당하는 항목을 이어붙이는 형태
+        //입력한 숫자를 모델의 데이터 형식에 맞게 조정한다.
+        //ex) 만삭 1명, 유산 1명,
         var femininityText = ""
         var et = womanFragment.findViewById<EditText>(R.id.TeditText)
+
+        //사용자가 입력한 항목에 대해서만 문자열을 더해준다.
         if (et.text.isNotBlank()) femininityText += "만삭 ${et.text}명,"
 
         et = womanFragment.findViewById(R.id.PeditText)
@@ -173,65 +180,52 @@ class BasicExamActivity : AppCompatActivity() {
 
         et = womanFragment.findViewById(R.id.LeditText)
         if (et.text.isNotBlank()) femininityText += " 생존 ${et.text}명,"
-        member.femininity = femininityText
 
-        /*
-        if (member.drug.equals(""))
-            member.drug = "없음"
+        member.femininity = femininityText //완성된 String을 저장
 
-        if (member.social.equals(""))
-            member.social = "없음"
 
-        if (member.family.equals(""))
-            member.family = "없음"
-
-        if (member.trauma.equals(""))
-            member.trauma = "없음"
-
-        if (member.femininity.equals(""))
-            member.femininity = "없음"
-*/
     }
 
 
 
     private fun saveMember2Server(member_: Member) { //서버에 멤버 정보 저장
-        val retrofit = RetrofitClass.getInstance()
+        val retrofit = RetrofitClass.getInstance() //http통신을 위해 retrofit 인스턴스를 가져온다(싱글톤)
         val memberService = retrofit.create(MemberService::class.java)
 
-        //isRevise인 경우에 PUT으로 정보 수정 추가해야함
-        if (isRevise) {
+
+        if (isRevise) { //기초문진을 수정하는 경우
+            //유저 id 경로에 갱신된 Member 클래스를 전송한다.
             memberService.reviseProfile(member_.id, getUpdateMember(member_)).enqueue(object : Callback<UpdateMemberResponse> {
                 override fun onResponse(call: Call<UpdateMemberResponse>, response: Response<UpdateMemberResponse>) {
-                    if (response.isSuccessful) {
-                        realConfirm() //id가 수정된 member를 SP에 저장
+                    if (response.isSuccessful) { //통신이 성공적이라면
+                        realConfirm() //수정된 정보를 로컬에도 적용
                     }
                 }
-                override fun onFailure(call: Call<UpdateMemberResponse>, t: Throwable) {
-                    println("그냥 안됨")
+                override fun onFailure(call: Call<UpdateMemberResponse>, t: Throwable) { //통신에 실패
+                    Toast.makeText(App.context(), "서버 통신 에러", Toast.LENGTH_SHORT).show() //메세지를 띄워준다
                 }
             })
         }
-        else {
+        else { //처음 기초문진을 작성하는 경우 (회원가입하는 경우)
+            //Member 클래스를 전송한다.
             memberService.setProfile(member_).enqueue(object : Callback<Member> {
                 override fun onResponse(call: Call<Member>, response: Response<Member>) {
-                    if (response.isSuccessful) {
+                    if (response.isSuccessful) { //통신이 성공적이라면
                         //생성 시 id는 0. 서버에 POST로 생성 후 부여 받는 id로 교체
                         member.id = response.body()!!.id
-                        realConfirm() //id가 수정된 member를 SP에 저장
+                        realConfirm()  //수정된 정보를 로컬에도 적용
                     }
                 }
                 override fun onFailure(call: Call<Member>, t: Throwable) {
                     Toast.makeText(App.context(), "서버 통신 에러", Toast.LENGTH_SHORT).show()
-                    realConfirm() //테스트로 넘어가기 나중에 지워야함~
+                    //realConfirm()
+                    //개발 중 테스트로 넘어가도록 설정
                 }
             })
         }
-
-
     }
 
-    private fun setConfirmDialogue() { //구현 예정
+    private fun setData() {
         //회원 정보를 마친다는 거시기랑 진짜 확인 하면 서버에 저장
         setBasicExam() //기초 문진을 member에 저장
         saveMember2Server(member) //서버에 member 정보 저장
